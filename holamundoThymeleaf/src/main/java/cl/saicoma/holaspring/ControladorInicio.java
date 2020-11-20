43530fa7c210d939f8bfab7c1c257ed42e5cb674
Mario Gutierrez Sanhueza <mgutiesa@everis.com>
2020-11-20 15:37:47 -0300

    avanze 20-11

--- holamundoThymeleaf/src/main/java/cl/saicoma/holaspring/ControladorInicio.java
+++ holamundoThymeleaf/src/main/java/cl/saicoma/holaspring/ControladorInicio.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 @Controller
 @Slf4j
@@ -18,17 +21,29 @@ public class ControladorInicio {
     public String inicio(Model model) {
         String mensaje = "Hola mundo con Thymeleaf";
         Persona persona = new Persona();
-
         persona.setNombre("Mario");
         persona.setApellido("Gutierrez");
         persona.setEmail("mgutiers3012@gmail.com");
         persona.setTelefono("123456789");
 
-        log.info("controlador Spring MVC");
+        Persona persona2 = new Persona();
+        persona2.setNombre("Karla");
+        persona2.setApellido("Gomez");
+        persona2.setEmail("KGomez@mail.com");
+        persona2.setTelefono("123456789");
+
+        // var personas = new ArrayList<>();
+        //personas.add(persona);
+        //personas.add(persona2);
+
+        //var personas = Arrays.asList(persona, persona2);
+        var personas = new ArrayList<>();
+       // log.info("controlador Spring MVC");
 
         model.addAttribute("mensaje", mensaje);
         model.addAttribute("saludo", saludo);
-        model.addAttribute("persona", persona);
+       // model.addAttribute("persona", persona);
+        model.addAttribute("personas", personas);
         return "index";
     }
 
